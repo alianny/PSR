81b3e3d55cb2d6873698ecb3989c21ebe50b6280
alianny <alianny@okstate.edu>
2018-03-19 17:15:31 -0500

    Work Update
    Python Code Mostly Complete. Working on console application now.

--- PSR.py
+++ PSR.py
@@ -1,4 +1,5 @@
 import sys
+import re
 from selenium import webdriver
 import time
 from lxml import html
@@ -23,31 +24,14 @@ def main():
     print "Launching Chrome Browser..."
     # specify the url
     WebBrowser = webdriver.Chrome()
+
     WebBrowser.get(sys.argv[1])
-    originalSource = WebBrowser.page_source
-    
-    print "Updating Website..."
-    putQuotes = '''%s'''% sys.argv[2]
-    
-    #//*[@id="ddlTimeFrame"]/option[1]
-    WebBrowser.find_element_by_xpath(putQuotes).click()
-    
-    print "Waiting on Update... # NEEDS WORK"
-    while (originalSource == WebBrowser.page_source):
-        print "Waiting half of second..."
-    time.sleep(.5)
-    
-    print "Extracting Table...-***NEEDS WORKS***"
-    
-    tree = html.fromstring(WebBrowser.page_source)
-    table = tree.xpath("""//*[@id="quotes_content_left_pnlAJAX"]/table/tbody//text()""", smart_strings=False)
-    if '\n' in table:
-        table = table.replace('\n', ' ')
-    if ' ' in table:
-        table = table.replace(' ', '')
 
-    print table
-
+    print "Going to NASDAQ..."
+    #Update if other website ever needed
+    quote_list = nasdaq(sys.argv[2], WebBrowser )
+    for m in quote_list:
+        print m
     
     print "Creating CSV FILE..."
     # query the website and return the html to the variable 'page'
@@ -63,12 +47,36 @@ def main():
     #price = price_box.text
     #print price_box
 
-
     # open a csv file with append, so old data will not be erased
     with open('index.csv', 'a') as csv_file:
        writer = csv.writer(csv_file)
-       writer.writerow([table])
+       writer.writerow([quote_list])
 
+def nasdaq( sysArugments, Web ):
+
+    originalSource = Web.page_source
+
+    print "Updating Website..."
+    putQuotes = '''%s'''% sysArugments
+    find_option_number = re.findall(r'\d+', sysArugments)
+    waitTime = .5 * float(find_option_number[0])
+    #//*[@id="ddlTimeFrame"]/option[1]
+    Web.find_element_by_xpath(putQuotes).click()
+
+    print "Waiting on %s ms for website update..." % waitTime
+    time.sleep(waitTime)
+    
+    print "Extracting Table...-***NEEDS WORKS***"
+    
+    tree = html.fromstring(Web.page_source)
+    table = tree.xpath("""//*[@id="quotes_content_left_pnlAJAX"]/table/tbody//text()""", smart_strings=False)
+
+    table = [number.replace('\n','') for number in table]
+    table = [number.replace("'",'') for number in table]
+    table = [number.replace(",",'') for number in table]
+    table = [number.replace(" ",'') for number in table]
+    str_list = filter(None, table)
+    return str_list
 
 if __name__ == '__main__':
     main()
